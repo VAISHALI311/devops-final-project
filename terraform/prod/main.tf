037bdf99b2a485fe34640441a36e0cfaaf595def
VAISHALI311 <vaishu50005@gmail.com>
2026-05-03 11:47:13 +0530

    Create main.tf

---
+++ terraform/prod/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "prod_server" {
+  ami           = "ami-091138d0f0d41ff90"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "prod-server"
+    Env  = "prod"
+  }
+}
